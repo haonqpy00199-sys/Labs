1e7bb304405d9bd8485f82ee105225b4f17acdad
haonqpy00199-sys <haonqpy00199@gmail.com>
2025-09-23 14:12:59 +0700

    Lab2

---
+++ Lab_2/Lab2/src/main/java/SharerServlet.java
@@ -0,0 +1,26 @@
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/home/index")
+public class SharerServlet  extends HttpServlet{
+@Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+  throws ServletException, IOException{
+	// set Attribute
+	req.setAttribute("message","Welcome to FPT Polytechnic!");
+	req.setAttribute("now", new Date());
+	
+	req.getRequestDispatcher("/views/page.jsp").forward(req,resp);
+  }
+}
